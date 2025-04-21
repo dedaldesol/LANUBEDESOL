cee65f3bdfcfcc78803b852b161af1e9b4aeae1a
dedaldesol <mariasoleal14@gmail.com>
2025-04-21 17:39:16 -0300

    botn de java interactivo

--- script.js
+++ script.js
@@ -3,6 +3,6 @@ const message = document.getElementById("dynamic-message");
 // Agrega un evento al botón
 button.addEventListener("click", () => {
     // Cambia el contenido del mensaje y lo muestra
-    message.textContent = "¡Gracias por tu interés! Estoy emocionada de seguir aprendiendo y creciendo en el mundo de la ciencia de datos”;
+    message.textContent = "¡Gracias por tu interés! Estoy emocionada de seguir aprendiendo y creciendo en el mundo de la ciencia de datos";
     message.style.display = "block"; // Muestra el mensaje
-    });
+});
